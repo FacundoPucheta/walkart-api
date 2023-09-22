e2ec794382a201ce4108c5a3dd707d90ef470065
Facundo Pucheta <facundopucheta94@gmail.com>
2023-09-22 20:08:52 -0300

    refactor(/src/models): models collection names

--- src/models/creationsModel.js
+++ src/models/creationsModel.js
@@ -2,38 +2,37 @@ const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose; // to be used on Schema Creation later
 
-const creationsSchema = new Schema({
-  // create new Schema => Define new model?
-  
+const creationsSchema = new Schema(
+  {
+    // create new Schema => Define new model?
+
     name: {
       type: String,
       required: false,
-      default: "Personalizada"
-      
+      default: "Personalizada",
     },
     image: {
       type: String,
       required: false,
-      
     },
     description: {
       type: String,
       required: [true, "Description is required"],
-      
     },
     category: {
       type: String,
       required: [true, "Category is required"],
-      
     },
     price: {
       type: Number,
       required: [true, "Price is required"],
-      
     },
-  
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Creations = model("creaciones", creationsSchema); // Create new model.
+const Creations = model("Creation", creationsSchema); // Create new model.
 
 module.exports = Creations;
